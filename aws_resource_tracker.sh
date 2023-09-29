1419379ebb595bdbbb6505933b784dcdeb483ec2
Anshul Gaikwad <111438824+AnshuLJGaikwad@users.noreply.github.com>
2023-09-29 23:08:18 +0530

    Update aws_resource_tracker.sh

--- aws_resource_tracker.sh
+++ aws_resource_tracker.sh
@@ -1,33 +1,29 @@
-default"
-                    },
-                    "PrivateDnsName": "ip-172-31-93-22.ec2.internal",
-                    "PrivateIpAddress": "172.31.93.22",
-                    "ProductCodes": [],
-                    "PublicDnsName": "",
-                    "State": {
-                        "Code": 80,
-                        "Name": "stopped"
-                    },
-                    "StateTransitionReason": "User initiated (2023-09-26 13:20:55 GMT)",
-                    "SubnetId": "subnet-0e99abad258568575",
-                    "VpcId": "vpc-0179be63faf0e1750",
-                    "Architecture": "x86_64",
-                    "BlockDeviceMappings": [
-                        {
-                            "DeviceName": "/dev/sda1",
-                            "Ebs": {
-                                "AttachTime": "2023-09-26T09:21:06.000Z",
-                                "DeleteOnTermination": true,
-                                "Status": "attached",
-                                "VolumeId": "vol-0c9a19ee5c698e457"
-                            }
-                        }
-                    ],
-                    "ClientToken": "7688ed0b-3a96-46d8-9d7d-5e6699d85983",
-                    "EbsOptimized": false,
-                    "EnaSupport": true,
-                    "Hypervisor": "xen",
-                    "NetworkInterfaces": [
-                        {
-                            "Attachment": {
-                                "AttachTime":
+#!/bin/bash
+
+###########
+# Author: Anshul
+# Date: 15th-September, 2023
+# Version: v1
+# This script will report the AWS resource usage
+###########
+
+# AWS S3
+# AWS EC2
+# AWS Lambda
+# AWS IAM Users
+
+# list S3 buckets
+aws s3 ls
+echo "Print list of s3 buckets"
+
+# list EC2 instances
+aws ec2 describe-instances
+echo "Print list of ec2 instances"
+
+# list aws lambda functions
+aws lambda list-functions
+echo "Print list of lambda functions"
+
+# list IAM users
+aws iam list-users
+echo "Print list of IAM users"
