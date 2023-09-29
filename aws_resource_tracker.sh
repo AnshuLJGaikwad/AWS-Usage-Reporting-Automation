326e2e65cc94f1c5699322c8fb47a221fbabdbc9
Anshul Gaikwad <111438824+AnshuLJGaikwad@users.noreply.github.com>
2023-09-29 23:04:51 +0530

    Create aws_resource_tracker.sh
    This is the script file which includes basic commands to get AWS usage as output

---
+++ aws_resource_tracker.sh
@@ -0,0 +1,33 @@
+default"
+                    },
+                    "PrivateDnsName": "ip-172-31-93-22.ec2.internal",
+                    "PrivateIpAddress": "172.31.93.22",
+                    "ProductCodes": [],
+                    "PublicDnsName": "",
+                    "State": {
+                        "Code": 80,
+                        "Name": "stopped"
+                    },
+                    "StateTransitionReason": "User initiated (2023-09-26 13:20:55 GMT)",
+                    "SubnetId": "subnet-0e99abad258568575",
+                    "VpcId": "vpc-0179be63faf0e1750",
+                    "Architecture": "x86_64",
+                    "BlockDeviceMappings": [
+                        {
+                            "DeviceName": "/dev/sda1",
+                            "Ebs": {
+                                "AttachTime": "2023-09-26T09:21:06.000Z",
+                                "DeleteOnTermination": true,
+                                "Status": "attached",
+                                "VolumeId": "vol-0c9a19ee5c698e457"
+                            }
+                        }
+                    ],
+                    "ClientToken": "7688ed0b-3a96-46d8-9d7d-5e6699d85983",
+                    "EbsOptimized": false,
+                    "EnaSupport": true,
+                    "Hypervisor": "xen",
+                    "NetworkInterfaces": [
+                        {
+                            "Attachment": {
+                                "AttachTime":
